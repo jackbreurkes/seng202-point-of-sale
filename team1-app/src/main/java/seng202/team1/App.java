85993927edab990355a311e281e38113c3bcbba1
Jack van Heugten Breurkes <jack.breurkes@gmail.com>
2019-08-02 11:27:06 +1200

    added intellij project folder and maven files

---
+++ team1-app/src/main/java/seng202/team1/App.java
@@ -0,0 +1,21 @@
+package seng202.team1;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        MyObject myObject = new MyObject();
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(myObject);
+
+        System.out.println(jsonString);
+    }
+}
