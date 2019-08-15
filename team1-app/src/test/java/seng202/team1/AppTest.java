b1acca27effacb38f09973a6996569758251db45
Jack van Heugten Breurkes <jack.breurkes@gmail.com>
2019-08-15 20:44:11 +1200

    updated the gitignore and pom file with Jack Steel's changes. also changed apptest.

--- team1-app/src/test/java/seng202/team1/AppTest.java
+++ team1-app/src/test/java/seng202/team1/AppTest.java
@@ -1,39 +1,19 @@
 package seng202.team1;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
     public void testApp()
     {
-        assertTrue( true );
+        assertTrue(  true );
     }
     
     public void jacksTest()
