5f8e28d531bafb420bee9387698c61e535fbe70e
David Turton <djt112@uclive.ac.nz>
2019-08-24 22:07:59 +1200

    Added some very basic JavaDoc to explain the functions in the Order Class, added a new function to check the current OrderStatus of an order. Updated test case for the Order class to test more scenarios.

--- team1-app/src/main/java/seng202/team1/model/Order.java
+++ team1-app/src/main/java/seng202/team1/model/Order.java
@@ -25,6 +25,7 @@ public class Order {
 
     /**
      * @param item
+     * adds a single instance of the specified item to the foodItems list
      */
     public void addItem(FoodItem item) {
         // TODO implement here
@@ -32,37 +33,46 @@ public class Order {
 
     /**
      * @param item
+     * removes a single instance of the specified item from the foodItems list
      */
     public void removeItem(FoodItem item) {
         // TODO implement here
     }
 
     /**
-     * 
+     * changes the status of the order to cancelled, (registers it in the database?)
      */
     public void cancelOrder() {
         // TODO implement here
     }
 
     /**
-     * 
+     * updates the status of the order to refunded (returns the amount of refunded money calculated by components?)
      */
     public void refundOrder() {
         // TODO implement here
     }
 
     /**
-     *
+     * changes the status of the order to complete, (registers it in the database?)
      */
     public void completeOrder() {
         // TODO implement here
     }
 
     /**
-     *
+     * returns the contents of the order in list form
      */
     public List getOrderContents() {
         // TODO implement here
         return foodItems;
+    }
+
+    /**
+     * returns the current status of the order
+     */
+    public OrderStatus getOrderStatus() {
+        // TODO implement here
+        return status;
     }
 }
--- team1-app/src/test/java/seng202/team1/data/OrderTest.java
+++ team1-app/src/test/java/seng202/team1/data/OrderTest.java
@@ -17,8 +17,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrderTest {
 
     Order testOrder;
-    FoodItem testItem;
-    FoodItem testItem2;
+    SuppliedFoodItem testItem;
+    SuppliedFoodItem testItem2;
+    SuppliedFoodItem testItem3;
     List<FoodItem> testList;
 
 
@@ -32,8 +33,10 @@ class OrderTest {
     @Test
     void testGetOrderContents() {
         assertNull(testOrder.getOrderContents());
-
-        // TODO add more here for if items exist
+        testOrder.addItem(testItem);
+        testList = new ArrayList<FoodItem>();
+        testList.add(testItem);
+        assertEquals(testOrder.getOrderContents(), testList);
     }
 
     @Test
@@ -51,7 +54,16 @@ class OrderTest {
         testOrder.addItem(testItem2);
         assertEquals(testOrder.getOrderContents(), testList);
 
-        // TODO test adding invalid items, ensuring that more than one of the same item can be added
+        //try to add another instance of testItem and ensure it correctly enters the list
+        testList.add(testItem);
+        testOrder.addItem(testItem);
+        assertEquals(testOrder.getOrderContents(), testList);
+
+        //adds an item that doesn't fit proper criteria of an item
+        testItem3 = new SuppliedFoodItem("COD\u2202", "Test Name", UnitType.COUNT);
+        assertThrows(IllegalArgumentException.class, () -> {
+            testOrder.addItem(testItem);
+        });
     }
 
     @Test
@@ -72,25 +84,48 @@ class OrderTest {
             testOrder.removeItem(testItem);
         });
 
-        // TODO don't just check for empty orders, check non-existant item
-        // TODO check you can remove an item twice if its added twice etc
+        //tests that removing an item only removes one instance of said item
+        testOrder.addItem(testItem);
+        testOrder.addItem(testItem);
+        testOrder.removeItem(testItem);
+        assertEquals(testOrder.getOrderContents(), testList);
+
+        //checks that removing the item again will make the order empty
+        testOrder.removeItem(testItem);
+        assertEquals(testOrder.getOrderContents(), null);
+
+        //tests that an item that is not in the list cannot be removed
+        testItem2 = new SuppliedFoodItem("TESB", "Test Item 2", UnitType.COUNT);
+        assertThrows(IllegalArgumentException.class, () -> {
+            testOrder.removeItem(testItem2);
+        });
     }
 
     @Test
     void testCancelOrder(){
-        //TODO
+        //TODO come up with some more tests, write a custom exception for whe an order is cancelled twice
         testOrder.addItem(testItem);
-        //make a second item and add it to the order, check the order is as expected
         testItem2 = new SuppliedFoodItem("TESB", "Test Item 2", UnitType.COUNT);
         testOrder.addItem(testItem2);
         testOrder.cancelOrder();
-
-        // TODO a cancelled order will still exist. Think about OrderStatus maybe
+        // TODO specify how the order status works (is it a string, etc)
+        // checks the status of the order has correctly been set to cancelled
+        assertEquals(testOrder.getOrderStatus(), "Cancelled");
 
         //makes sure an order can't be cancelled twice
         assertThrows(IllegalArgumentException.class, () -> {
             testOrder.cancelOrder();
             // TODO maybe write a custom exception for this?
+        });
+
+        //checks a cancelled order cannot have items added to it
+        assertThrows(IllegalArgumentException.class, () -> {
+            testOrder.addItem(testItem);
+        });
+
+        //checks a cancelled order cannot have items removed from it
+        assertThrows(IllegalArgumentException.class, () -> {
+            testOrder.removeItem(testItem);
         });
 
         //Makes sure a cancelled order cannot be completed
@@ -103,15 +138,25 @@ class OrderTest {
 
     @Test
     void testCompleteOrder(){
-        //TODO
+        //TODO come up with some more tests
         testOrder.addItem(testItem);
         testItem2 = new SuppliedFoodItem("TESB", "Test Item 2", UnitType.COUNT);
         testOrder.addItem(testItem2);
         testOrder.completeOrder();
-
-        // TODO ues OrderStatus
+        // checks the status of the order has correctly been set to completed
+        assertEquals(testOrder.getOrderStatus(), "Complete");
 
         //Makes sure an order cannot be completed twice
+        assertThrows(IllegalArgumentException.class, () -> {
+            testOrder.completeOrder();
+        });
+
+        //checks a completed order cannot have items removed from it
+        assertThrows(IllegalArgumentException.class, () -> {
+            testOrder.removeItem(testItem);
+        });
+
+        //Makes sure a completed order cannot be cancelled
         assertThrows(IllegalArgumentException.class, () -> {
             testOrder.completeOrder();
         });
@@ -123,6 +168,9 @@ class OrderTest {
         //sets a starting amount of money
         double cash = 20;
         testOrder.completeOrder();
+        testOrder.refundOrder();
+        // checks the status of the order has correctly been set to refunded
+        assertEquals(testOrder.getOrderStatus(), "Refunded");
 
         // TODO more stuff with orderstatus here, some more tests
     }
