a0a8b6794688d6364a3f8bb844ec2c4857d75f25
Euan Widjaja <ewi45@uclive.ac.nz>
2019-08-29 22:47:24 +1200

    Fixed Menu code that caused broken pipe

--- team1-app/src/main/java/seng202/team1/model/Menu.java
+++ team1-app/src/main/java/seng202/team1/model/Menu.java
@@ -1,5 +1,7 @@
 package seng202.team1.model;
 
+import seng202.team1.util.UnitType;
+
 import java.util.*;
 
 /**
@@ -7,43 +9,37 @@ import java.util.*;
  */
 public class Menu {
 
+    private String name;
+    private List<FoodItem> items = new ArrayList<FoodItem>();
+
     /**
      * Default constructor
      */
     public Menu() {
     }
 
-    private String name;
-    private List<FoodItem> items;
-
 
     /**
-     * adds a FoodItem to the menu
-     * @param item
-     */
-    public void addItem(FoodItem item) {
-        // TODO implement here
-    }
-
-    /**
-     * removes a FoodItem from the menu
-     * @param item
-     */
-    public void removeItem(FoodItem item) {
-        // TODO implement here
-    }
-
-    /**
-     * obtains menu name
-     * @param name
+     * sets name of menu
+     * @param name String name
      */
     public void setMenuName(String name) {
         this.name = name;
     }
 
+
+    /**
+     * obtains name of menu
+     * @return a String value
+     */
+    public String getMenuName() {
+        return name;
+    }
+
+
     /**
      * obtains a list of food items in the menu
-     * @return
+     * @return a List<FoodItem>
      */
     public List<FoodItem> getMenuItems() {
         return items;
@@ -51,12 +47,35 @@ public class Menu {
 
 
     /**
-     *  erases the content of a menu
+     * obtains length of a menu
+     * @return an int
      */
-    public void removeMenu() {
-        items.removeAll(items);
+    public int getMenuLength() {
+        return items.size();
     }
 
 
+    /**
+     * adds a FoodItem to the menu
+     * @param item FoodItem
+     */
+    public void addItem(FoodItem item) {
+        items.add(item);
+    }
 
+
+    /**
+     * removes a FoodItem from the menu
+     * @param item FoodItem
+     */
+    public void removeItem(FoodItem item) {
+        items.remove(item);
+    }
+
+    /**
+     *  erases the content of a menu
+     */
+    public void removeMenu() {
+        items.clear();
+    }
 }
