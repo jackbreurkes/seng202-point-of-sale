a6d0ac9527e3219600d7ab8d1f0db0bbbfc3fb3f
Richard <rvo16>
2019-09-01 22:24:39 +1200

    Add new editData.fxml scene and create new EditDataController class. Partially implemented EditDataController class.

---
+++ team1-app/src/main/java/seng202/team1/GUI/EditDataController.java
@@ -0,0 +1,34 @@
+package seng202.team1.GUI;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EditDataController {
+
+
+
+    /**
+     * Changes scene back to import with table
+     */
+    private void changeSceneToImport(javafx.event.ActionEvent event) throws IOException {
+        Parent importParent = FXMLLoader.load(getClass().getResource("import.fxml"));
+        Scene importScene = new Scene(importParent);
+
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(importScene);
+        window.show();
+    }
+
+    /**
+     * goes back to the import table screen called by the back button
+     */
+    public void goBack(javafx.event.ActionEvent event) throws IOException {
+        changeSceneToImport(event);
+    }
+}
