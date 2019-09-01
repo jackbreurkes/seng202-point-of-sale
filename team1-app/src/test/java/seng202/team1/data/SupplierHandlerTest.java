6acff753856b671848a27d2d52ab540fc108e942
Euan Widjaja <ewi45@uclive.ac.nz>
2019-09-01 12:46:35 +1200

    Created SupplierHandlerTest for testing. Haven't really added anything to it.

---
+++ team1-app/src/test/java/seng202/team1/data/SupplierHandlerTest.java
@@ -0,0 +1,20 @@
+package seng202.team1.data;
+
+import org.junit.jupiter.api.Test;
+
+public class SupplierHandlerTest {
+
+    @Test
+    void parseInput() {
+    }
+
+    @Test
+    void parsedDoc() {
+    }
+
+    @Test
+    void supplierHandlerTest(){
+        FoodItemHandler handler = new FoodItemHandler("src/test/resources/xml/TESTXML1.xml", true);
+        handler.parseInput();
+    }
+}
