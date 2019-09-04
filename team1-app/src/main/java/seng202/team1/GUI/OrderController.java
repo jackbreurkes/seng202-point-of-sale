32ead9c9d015957062cfabf0ec5c054575e96823
rvo16 <rvo16@uclive.ac.nz>
2019-09-04 14:52:35 +1200

    Fix merge issues on lab computer from commit on home computer.

---
+++ team1-app/src/main/java/seng202/team1/GUI/OrderController.java
@@ -0,0 +1,47 @@
+package seng202.team1.GUI;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class OrderController {
+
+    /**
+     * When this methods is called, it will change the scene to datatype controller view
+     */
+    public void changeSceneToImport(javafx.event.ActionEvent event) throws IOException
+    {
+        Parent editDataParent = FXMLLoader.load(getClass().getResource("order.fxml"));
+        Scene editDataScene = new Scene(editDataParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //This line sets the screen title
+        window.setTitle("ROSEMARY | Order Screen");
+
+        window.setScene(editDataScene);
+        window.show();
+    }
+
+    /**
+     * When this methods is called, it will change the scene to datatype controller view
+     */
+    public void changeSceneToEditData(javafx.event.ActionEvent event) throws IOException
+    {
+        Parent editDataParent = FXMLLoader.load(getClass().getResource("editData.fxml"));
+        Scene editDataScene = new Scene(editDataParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //This line sets the screen title
+        window.setTitle("ROSEMARY | Edit Data Screen");
+
+        window.setScene(editDataScene);
+        window.show();
+    }
+
+}
