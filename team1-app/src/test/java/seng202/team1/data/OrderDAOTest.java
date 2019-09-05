906ce1e8c7e311505a5e93d54f54b58dfdd929a4
David Turton <djt112@uclive.ac.nz>
2019-09-05 19:30:51 +1200

    Created the OrderDAO test class and wrote up the framework for the tests.

---
+++ team1-app/src/test/java/seng202/team1/data/OrderDAOTest.java
@@ -0,0 +1,46 @@
+package seng202.team1.data;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import seng202.team1.model.Order;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Disabled
+class OrderDAOTest {
+
+    private OrderDAO orderStorage;
+    private Order order;
+
+    @BeforeEach
+    void setupStorage() {
+        //TODO set this up
+    }
+
+    @Test
+    void getAllOrders() {
+
+    }
+
+    @Test
+    void getOrderByCode() {
+
+    }
+
+    @Test
+    void addOrder() {
+
+    }
+
+    @Test
+    void updateOrder() {
+
+    }
+
+    @Test
+    void removeOrder() {
+
+    }
+}
