ff54b62fe11ec7cf6f35d84b10a90c59a943df1e
Djt112 <djt112@uclive.ac.nz>
2019-09-05 19:49:57 +1200

    Created the OrderDAO test class and wrote up the framework for the tests, wrote up some tests for the getAllOrders test

--- team1-app/src/test/java/seng202/team1/data/OrderDAOTest.java
+++ team1-app/src/test/java/seng202/team1/data/OrderDAOTest.java
@@ -12,21 +12,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrderDAOTest {
 
     private OrderDAO orderStorage;
-    private Order order;
+    private Order testOrder;
 
     @BeforeEach
     void setupStorage() {
         //TODO set this up
+        testOrder = new Order();
     }
 
     @Test
     void getAllOrders() {
+        //test an empty set of orders has size 0
+        Set<Order> items = orderStorage.getAllOrders();
+        assertEquals(0, items.size());
+
+        //TODO Jack used MemoryStorage.getInstance here, maybe refactor this to use that.
+        orderStorage.addOrder(testOrder);
+
+        assertEquals(1, items.size());
+        assertTrue(items.contains(testOrder));
 
     }
 
     @Test
     void getOrderByCode() {
-
+        //TODO Need to setup a system for Order Codes.
     }
 
     @Test
