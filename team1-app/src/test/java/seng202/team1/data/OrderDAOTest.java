3fa864407f4a9183cf533a0887e21c95958e5e47
David Turton <djt112@uclive.ac.nz>
2019-09-07 14:05:32 +1200

    Added some more tests to the OrderDAO test class.

--- team1-app/src/test/java/seng202/team1/data/OrderDAOTest.java
+++ team1-app/src/test/java/seng202/team1/data/OrderDAOTest.java
@@ -3,8 +3,10 @@ package seng202.team1.data;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import seng202.team1.model.FoodItem;
 import seng202.team1.model.Order;
 import seng202.team1.util.InvalidDataCodeException;
+import seng202.team1.util.UnitType;
 
 import java.util.Set;
 
@@ -14,6 +16,8 @@ class OrderDAOTest {
 
     private OrderDAO orderStorage;
     private Order testOrder;
+    private FoodItem testItem = new FoodItem("TEST", "Test Item", UnitType.COUNT);
+    private FoodItem testItem2 = new FoodItem("TES2", "Another Test Item", UnitType.COUNT);
 
     @BeforeEach
     void setupStorage() {
@@ -42,6 +46,13 @@ class OrderDAOTest {
 
     @Test
     void TestAddOrder() {
+
+        //cant add an empty order.
+        assertThrows(NullPointerException.class, () -> {
+            orderStorage.addOrder(testOrder);
+        });
+
+        testOrder.addItem(testItem);
         orderStorage.addOrder(testOrder);
         //test for when order-code is implemented in the order class
         //assertEquals(testOrder, orderStorage.getOrderByCode(testOrder.getCode()));
@@ -60,19 +71,47 @@ class OrderDAOTest {
     @Test
     void testUpdateOrder() {
         //TODO update the order using the provided order code? still gotta get order codes done
+
+        /*
+        //adds an order to storage, then updates the order and calls updateOrder, then checks the order was correctly updated
+        testOrder.addItem(testItem);
+        orderStorage.addOrder(testOrder);
+
+        testOrder.addItem(testItem2);
+        orderStorage.updateOrder(testOrder);
+        assertEquals(testOrder, orderStorage.getOrderByCode(testOrder.getCode()));
+
+        //makes sure you cant update a null order
+        assertThrows(NullPointerException.class, () -> {
+            orderStorage.updateOrder(null);
+        });
+
+         */
     }
 
     @Test
     void testRemoveOrder() {
-        orderStorage.addOrder(testOrder);
         /*
+        //cant remove from an order that isnt in the system
+        assertThrows(NullPointerException.class, () -> {
+            orderStorage.removeOrder(testOrder.getCode());
+        });
+        orderStorage.addOrder(testOrder);
+
         //needs getCode to work
         orderStorage.removeOrder(testOrder.getCode());
-        assertNull(orderStorage.getOrderByCode(testOrder.getCode()));
+        assertNull(orderStorage.getOrderByCode(testOrder.getCode()));z
         //needs getCode to work
         assertThrows(InvalidDataCodeException.class, () -> {
             orderStorage.removeOrder(testOrder.getCode());
         });
         */
+
+        //cant remove from a null order
+        assertThrows(NullPointerException.class, () -> {
+            orderStorage.removeOrder(null);
+        });
+
+
     }
 }
