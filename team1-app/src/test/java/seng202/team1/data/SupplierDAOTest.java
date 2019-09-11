d9128c081f001ae55fec9c781761d223f6f9024a
Djt112 <djt112@uclive.ac.nz>
2019-09-11 21:24:25 +1200

    Created a test class for SupplierDAO

---
+++ team1-app/src/test/java/seng202/team1/data/SupplierDAOTest.java
@@ -0,0 +1,46 @@
+package seng202.team1.data;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplierDAOTest {
+    @BeforeEach
+    void setupStorage() {
+
+    }
+
+    @Test
+    void testGetAllSuppliers() {
+
+
+    }
+
+    @Test
+    void testGetSupplierByID() {
+
+
+    }
+
+    @Test
+    void testAddSupplier() {
+
+
+    }
+
+    @Test
+    void testUpdateSupplier() {
+
+
+    }
+
+    @Test
+    void testRemoveSupplier() {
+
+
+    }
+}
