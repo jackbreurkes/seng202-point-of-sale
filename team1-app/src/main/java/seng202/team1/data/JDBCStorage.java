0904aab919ec4be62e986eaaacaac044a0287263
Jack van Heugten Breurkes <jack.breurkes@gmail.com>
2019-09-17 19:35:17 +1200

    added a JDBCStorage class with some test database calls to show what we might use in our DB. added SQLite to our pom file dependency list.

---
+++ team1-app/src/main/java/seng202/team1/data/JDBCStorage.java
@@ -0,0 +1,107 @@
+package seng202.team1.data;
+
+import java.sql.*;
+import java.util.Date;
+
+public class JDBCStorage {
+    private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+
+    public static void createNewDatabase() {
+
+        String url = "jdbc:sqlite:test.db";
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:test.db";
+
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "    id integer PRIMARY KEY,\n"
+                + "    name text NOT NULL,\n"
+                + "    capacity real\n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Connect to the test.db database
+     *
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO warehouses(id,name,capacity) VALUES(?,?,?)";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, 1);
+            pstmt.setString(2, name);
+            pstmt.setDouble(3, capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void readDataBase() throws Exception {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
+
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM warehouses");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            //close();
+        }
+    }
+
+    public static void main(String[] args) {
+        JDBCStorage storage = new JDBCStorage();
+        storage.createNewDatabase();
+        storage.createNewTable();
+        storage.insert("tr", 3.33d);
+        try {
+            storage.readDataBase();
+        } catch (Exception e) {
+            System.out.println("error thrown:" + e);
+        }
+    }
+
+}
---
+++ team1-app/src/test/java/seng202/team1/data/JDBCStorageTest.java
@@ -0,0 +1,14 @@
+package seng202.team1.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JDBCStorageTest {
+
+    @Test
+    void testReadDataBase() {
+
+    }
+
+}
