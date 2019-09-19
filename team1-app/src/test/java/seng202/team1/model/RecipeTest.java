872d2bc13f81386c8cd88864a83ca1d7cf7e81f0
Phillip Kim <kbu34@uclive.ac.nz>
2019-09-19 15:03:53 +1200

    RecipeTest

--- team1-app/src/test/java/seng202/team1/model/RecipeTest.java
+++ team1-app/src/test/java/seng202/team1/model/RecipeTest.java
@@ -61,7 +61,29 @@ class RecipeTest {
 
 
         // Test if the amount of its product created is 0
+        Recipe testRecipe = new Recipe(new HashSet<>(), new HashSet<>(), new HashMap<>(), 0);
+        assertEquals(0, testRecipe.getAmountCreated());
+
         // Test ingredient amount does not have an entry for every ingredient
+        Set<FoodItem> ingredients1 = new HashSet<>();
+        ingredients1.add(new FoodItem("1111", "bun", UnitType.COUNT));
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Recipe(ingredients1, new HashSet<>(), new HashMap<>(), 1);
+        });
+
+        // Test ingredient amount does not have an entry for every addable ingredient
+        Set<FoodItem> ingredients2 = new HashSet<>();
+        ingredients2.add(new FoodItem("1111", "bun", UnitType.COUNT));
+        Map<String, Integer> ingredientAmounts = new HashMap<String, Integer>();
+        ingredientAmounts.put("1111", 1);
+        Set<FoodItem> addableIngredients = new HashSet<>();
+
+        addableIngredients.add(new FoodItem("2222", "tofu patty", UnitType.COUNT));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Recipe(ingredients2, addableIngredients, ingredientAmounts, 1);
+        });
+
         // Test everything works
 
     }
@@ -75,8 +97,39 @@ class RecipeTest {
         });
 
         // Pass food item that is not addable
+        FoodItem niceBun1 = new FoodItem("6969", "niceBun", UnitType.COUNT);
+        Set<FoodItem> ingredients1 = new HashSet<>();
+        Set<FoodItem> addableIngredients1 = new HashSet<>();
+
+        Map<String, Integer> ingredientAmounts1 = new HashMap<String, Integer>();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Recipe testRecipe1 = new Recipe(ingredients1, addableIngredients1, ingredientAmounts1, 0);
+            testRecipe1.addIngredient(niceBun1);
+        });
+
         // Pass a food item that is addable
+        FoodItem niceBun2 = new FoodItem("6969", "niceBun", UnitType.COUNT);
+        Set<FoodItem> ingredients2 = new HashSet<>();
+        Set<FoodItem> addableIngredients2 = new HashSet<>();
+
+        Map<String, Integer> ingredientAmounts2 = new HashMap<String, Integer>();
+        FoodItem tofuPatty2 = new FoodItem("2222", "tofu patty", UnitType.COUNT);
+        addableIngredients2.add(tofuPatty2);
+
+        Recipe testRecipe2 = new Recipe(ingredients2, addableIngredients2, ingredientAmounts2, 0);
+        testRecipe2.addIngredient(niceBun2);
+
+        Set<FoodItem> testSet = new HashSet<>();
+        testSet.add(niceBun2);
+        assertEquals(testSet, testRecipe2.getIngredients());
+
         // Adding a food item that has been previously removed
+        FoodItem durian = new FoodItem("3939", "durian", UnitType.COUNT);
+        Set<FoodItem> ingredients3 = new HashSet<>();
+        ingredients3.add(durian);
+        //Recipe testRecipe3 = new Recipe(ingredients3, new HashSet<>(), );
+
         // Adding a food item that is already in the recipe
     }
 
