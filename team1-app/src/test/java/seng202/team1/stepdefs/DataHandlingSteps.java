81d619e2b9be599b85761b8a9d28028fdaf7e590
Euan Widjaja <ewi45@uclive.ac.nz>
2019-09-22 21:54:03 +1200

    Modified DataHandlingSteps so that it tests both TESTXMLs (TESTXML1 and TESTXML2). Additionally modified some code in DataHandlingSteps so that it accommodates the testing of two different XML files; particularly, so that it resets the storage in every scenario. Also implemented a switch statement to choose between the two XML files and which to test.

--- team1-app/src/test/java/seng202/team1/stepdefs/DataHandlingSteps.java
+++ team1-app/src/test/java/seng202/team1/stepdefs/DataHandlingSteps.java
@@ -29,7 +29,9 @@ import static org.junit.Assert.assertEquals;
 public class DataHandlingSteps {
 
     String source;
+    String testName;
     List<FoodItem> expectedItems;
+    FoodItemDAO itemStorage;
 
     @Given("the user has a data {string} to upload")
     public void the_user_has_a_data_file_to_upload(String source) {
@@ -39,55 +41,86 @@ public class DataHandlingSteps {
 
     @And("the file is the right type and correctly formatted")
     public void the_file_is_the_right_type_and_correctly_formatted() {
-        assertEquals(source.contains(".xml"), true);
-        //throw new cucumber.api.PendingException();
+        if (source.contains("TESTXML1.xml")) {
+            testName = "TESTXML1";
+        } else if (source.contains("TESTXML2.xml")) {
+            testName = "TESTXML2";
+        } else {
+            throw new cucumber.api.PendingException(source + " not a valid directory");
+        }
     }
 
     @When("the user uploads the file")
     public void the_user_uploads_the_file() {
+        itemStorage = DAOFactory.getFoodItemDAO();
+        DAOFactory.resetInstances();
         try {
             UploadHandler.uploadFoodItems(source);
         } catch (IOException | SAXException e) {
             e.printStackTrace();
+            throw new cucumber.api.PendingException();
         }
-        //throw new cucumber.api.PendingException();
     }
 
+    // Might change this description to something else
+    // like data uploaded equal to test data?
+    // Not 100% sure what this test is meant to do.
     @Then("the system adds the new data to the current data")
     public void the_system_adds_the_new_data_to_the_current_data() {
         // Write code here that turns the phrase above into concrete actions
-        //UNSURE ABOUT THIS PART - WILL NEED TO ASK WHOEVER DID UPLOAD HANDLER
-
-        FoodItemDAO itemStorage = DAOFactory.getFoodItemDAO();
-
         List<FoodItem> items = new ArrayList<FoodItem>(itemStorage.getAllFoodItems());
         items.sort((item1, item2) -> item1.getCode().compareTo(item2.getCode()));
 
-        FoodItem beefburg = new FoodItem("BEEFBURG", "Hamburger", UnitType.GRAM);
-        beefburg.setCost(BigMoney.parse("NZD 21.00"));
-        beefburg.setCaloriesPerUnit(295);
-        FoodItem cheeseburg = new FoodItem("CHEESEBURG", "Cheeseburger", UnitType.GRAM);
-        cheeseburg.setCost(BigMoney.parse("NZD 22.00"));
-        cheeseburg.setCaloriesPerUnit(303);
-        FoodItem tofuburg = new FoodItem("TOFUBURG", "Tofu Burger", UnitType.GRAM);
-        tofuburg.setCost(BigMoney.parse("NZD 50.00"));
-        tofuburg.setCaloriesPerUnit(168.4);
-        tofuburg.setIsVegetarian(true);
-        FoodItem applesoda = new FoodItem("APPLESODA", "Sparkling Apple", UnitType.ML);
-        applesoda.setCost(BigMoney.parse("NZD 96.00"));
-        applesoda.setCaloriesPerUnit(87);
-        applesoda.setIsVegan(true);
-        applesoda.setIsGlutenFree(true);
-
-        expectedItems = Arrays.asList(
-                beefburg, cheeseburg, tofuburg, applesoda
-        );
-        expectedItems.sort((item1, item2) -> item1.getCode().compareTo(item2.getCode()));
+        switch (testName) {
+            case "TESTXML1":
+                FoodItem beefburg = new FoodItem("BEEFBURG", "Hamburger", UnitType.GRAM);
+                beefburg.setCost(BigMoney.parse("NZD 21.00"));
+                beefburg.setCaloriesPerUnit(295);
+                FoodItem cheeseburg = new FoodItem("CHEESEBURG", "Cheeseburger", UnitType.GRAM);
+                cheeseburg.setCost(BigMoney.parse("NZD 22.00"));
+                cheeseburg.setCaloriesPerUnit(303);
+                FoodItem tofuburg1 = new FoodItem("TOFUBURG", "Tofu Burger", UnitType.GRAM);
+                tofuburg1.setCost(BigMoney.parse("NZD 50.00"));
+                tofuburg1.setCaloriesPerUnit(168.4);
+                tofuburg1.setIsVegetarian(true);
+                FoodItem applesoda = new FoodItem("APPLESODA", "Sparkling Apple", UnitType.ML);
+                applesoda.setCost(BigMoney.parse("NZD 96.00"));
+                applesoda.setCaloriesPerUnit(87);
+                applesoda.setIsVegan(true);
+                applesoda.setIsGlutenFree(true);
+                expectedItems = Arrays.asList(
+                        beefburg, cheeseburg, tofuburg1, applesoda
+                );
+                expectedItems.sort((item1, item2) -> item1.getCode().compareTo(item2.getCode()));
+                break;
+            case "TESTXML2":
+                FoodItem banana = new FoodItem("BANANA", "Banana", UnitType.COUNT);
+                banana.setCost(BigMoney.parse("NZD 1.00"));
+                banana.setCaloriesPerUnit(100);
+                FoodItem applepie = new FoodItem("APPLEPIE", "Apple Pie", UnitType.COUNT);
+                applepie.setCost(BigMoney.parse("NZD 21.00"));
+                applepie.setCaloriesPerUnit(200);
+                FoodItem tofuburg2 = new FoodItem("TOFUBURG", "Tofu Burger", UnitType.GRAM);
+                tofuburg2.setCost(BigMoney.parse("NZD 50.00"));
+                tofuburg2.setCaloriesPerUnit(268.4);
+                tofuburg2.setIsVegetarian(true);
+                FoodItem water = new FoodItem("WATER", "Glass of water", UnitType.ML);
+                water.setCost(BigMoney.parse("NZD 200.00"));
+                water.setCaloriesPerUnit(0);
+                water.setIsVegan(true);
+                water.setIsGlutenFree(true);
+                expectedItems = Arrays.asList(
+                        banana, applepie, tofuburg2, water
+                );
+                expectedItems.sort((item1, item2) -> item1.getCode().compareTo(item2.getCode()));
+                break;
+            default:
+                throw new cucumber.api.PendingException(testName + " is an invalid file");
+        }
 
         Assertions.assertEquals(items.size(), expectedItems.size());
         Assertions.assertEquals(items, expectedItems);
 
-        //throw new cucumber.api.PendingException();
     }
 
     @Ignore
--- team1-app/src/test/resources/seng202/team1/features/datahandling.feature
+++ team1-app/src/test/resources/seng202/team1/features/datahandling.feature
@@ -7,3 +7,10 @@ Feature: User handles data using the app
     When the user uploads the file
     Then the system adds the new data to the current data
     And prompts the user to decide what to keep for each duplicate entry
+
+  Scenario: User uploads data from file
+    Given the user has a data "src/test/resources/xml/TESTXML2.xml" to upload
+    And the file is the right type and correctly formatted
+    When the user uploads the file
+    Then the system adds the new data to the current data
+    And prompts the user to decide what to keep for each duplicate entry
