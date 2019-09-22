f30740e07a29dec2ed1f5bd6239272de9dbf65b6
Euan Widjaja <ewi45@uclive.ac.nz>
2019-09-22 17:23:58 +1200

    Fixed broken pipe error. Found issue inside FoodItemHandlerTest and the TestXMLs since cost is now a part of FoodItem's DTD and I had not updated the test XML. Added trivial cost attributes on FoodItemHandlerTest for testing. Trivial as it does not really have an impact on functionality but just for consistency. This should work now.

--- team1-app/src/test/java/seng202/team1/data/FoodItemHandlerTest.java
+++ team1-app/src/test/java/seng202/team1/data/FoodItemHandlerTest.java
@@ -1,10 +1,14 @@
 package seng202.team1.data;
 
+import org.joda.money.BigMoney;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 import seng202.team1.model.FoodItem;
 import seng202.team1.util.UnitType;
 
@@ -20,6 +24,8 @@ class FoodItemHandlerTest {
 
     FoodItemHandler handler;
     String source;
+    DocumentBuilder builder;
+
 
 
     @BeforeEach
@@ -36,9 +42,34 @@ class FoodItemHandlerTest {
         // Create another document to parse and assert equals to.
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setValidating(true);
+
+        try {
+            builder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace(); // the parser configuration is set in this method only, so this shouldn't be a problem
+        }
+
+        builder.setEntityResolver((publicId, systemId) -> new InputSource(SupplierHandler.class.getResourceAsStream("/dtd/fooditems.dtd")));
+        builder.setErrorHandler(new ErrorHandler() {
+            @Override
+            public void error(SAXParseException exception) throws SAXException {
+                throw exception;
+            }
+
+            @Override
+            public void fatalError(SAXParseException exception) throws SAXException {
+                throw exception;
+            }
+
+            @Override
+            public void warning(SAXParseException exception) throws SAXException {
+                throw exception;
+            }
+        });
+
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document parsedDoc = builder.parse(source);
-        assertEquals(handler.parsedDoc().toString(), parsedDoc.toString());
+        Document parsedDocument = builder.parse(source);
+        assertEquals(handler.parsedDoc().toString(), parsedDocument.toString());
 
     }
 
@@ -52,13 +83,17 @@ class FoodItemHandlerTest {
         }
 
         FoodItem beefburg = new FoodItem("BEEFBURG", "Hamburger", UnitType.GRAM);
+        beefburg.setCost(BigMoney.parse("NZD 21.00"));
         beefburg.setCaloriesPerUnit(295);
         FoodItem cheeseburg = new FoodItem("CHEESEBURG", "Cheeseburger", UnitType.GRAM);
+        cheeseburg.setCost(BigMoney.parse("NZD 22.00"));
         cheeseburg.setCaloriesPerUnit(303);
         FoodItem tofuburg = new FoodItem("TOFUBURG", "Tofu Burger", UnitType.GRAM);
+        tofuburg.setCost(BigMoney.parse("NZD 50.00"));
         tofuburg.setCaloriesPerUnit(168.4);
         tofuburg.setIsVegetarian(true);
         FoodItem applesoda = new FoodItem("APPLESODA", "Sparkling Apple", UnitType.ML);
+        applesoda.setCost(BigMoney.parse("NZD 96.00"));
         applesoda.setCaloriesPerUnit(87);
         applesoda.setIsVegan(true);
         applesoda.setIsGlutenFree(true);
