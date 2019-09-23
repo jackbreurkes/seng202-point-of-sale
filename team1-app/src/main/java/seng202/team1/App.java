59b02ca15d09823bda29b741e0bd51a4cd38aae6
Jack van Heugten Breurkes <jack.breurkes@gmail.com>
2019-09-23 15:05:19 +1200

    deleted Main.java from GUI class

--- team1-app/src/main/java/seng202/team1/App.java
+++ team1-app/src/main/java/seng202/team1/App.java
@@ -1,6 +1,6 @@
 package seng202.team1;
 
-import seng202.team1.GUI.Main;
+import seng202.team1.GUI.SampleApplication;
 
 /**
  * Hello world!
@@ -10,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        Main.main(args);
+        SampleApplication.main(args);
     }
 }
