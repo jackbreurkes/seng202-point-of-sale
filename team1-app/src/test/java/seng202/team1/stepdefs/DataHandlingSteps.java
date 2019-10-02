6e3d789a515d7b3ff82a5078155a54defecdfb82
Euan Widjaja <ewi45@uclive.ac.nz>
2019-10-03 00:21:38 +1300

    Added a usable datahandling acceptance test case that (not) simply checks the price of a tofu burger. Code a little bit ugly, as database storage stores FoodItem cost as a BigMoney object and had to convert an int into string and concat that into a BigMoney with an "NZD " in the start, then parse that into a BigMoney. Not sure if there are other ways to do calculation with BigMoney.

--- team1-app/src/test/java/seng202/team1/stepdefs/DataHandlingSteps.java
+++ team1-app/src/test/java/seng202/team1/stepdefs/DataHandlingSteps.java
@@ -16,6 +16,7 @@ import seng202.team1.model.FoodItem;
 import seng202.team1.util.UnitType;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,97 +25,149 @@ import static org.junit.Assert.assertEquals;
 
 public class DataHandlingSteps {
 
-    String source;
-    String testName;
-    List<FoodItem> expectedItems;
-    FoodItemDAO itemStorage;
+        FoodItemDAO itemStorage;
+        FoodItem foodItem;
+        BigMoney foodItemCost;
 
-    @Given("the user has a data from directory {string} to upload")
-    public void the_user_has_a_data_from_directory_to_upload(String source) {
-        this.source = source;
-        //throw new cucumber.api.PendingException();
-    }
 
-    @And("the file is the right type and correctly formatted")
-    public void the_file_is_the_right_type_and_correctly_formatted() {
-        if (source.contains("TESTXML1.xml")) {
-            testName = "TESTXML1";
-        } else if (source.contains("TESTXML2.xml")) {
-            testName = "TESTXML2";
+    @Given("XML file from {string} is uploaded")
+    public void XML_file_from_directory_is_uploaded(String directory) {
+        if (directory.contains(".xml")) {
+            itemStorage = DAOFactory.getFoodItemDAO();
+            DAOFactory.resetInstances();
+            try {
+                UploadHandler.uploadFoodItems(directory);
+            } catch (IOException | SAXException e) {
+                throw new cucumber.api.PendingException(directory + " not a valid directory");
+            }
         } else {
-            throw new cucumber.api.PendingException(source + " not a valid directory");
+            throw new cucumber.api.PendingException(directory + " not a valid directory");
         }
     }
 
-    @When("the user uploads the file")
-    public void the_user_uploads_the_file() {
-        itemStorage = DAOFactory.getFoodItemDAO();
-        DAOFactory.resetInstances();
-        try {
-            UploadHandler.uploadFoodItems(source);
-        } catch (IOException | SAXException e) {
-            e.printStackTrace();
-            throw new cucumber.api.PendingException("Failed uploading " + testName);
+    @When("user wants to know the total cost of {int} {string}")
+    public void user_wants_to_know_the_total_cost_of(int count, String foodItemCode) {
+//        boolean valid = false;
+//        for (FoodItem foodItemStorage: itemStorage.getAllFoodItems()) {
+//            if (foodItemStorage.getCode() == foodItemCode) {
+//                valid = true;
+//                foodItem = foodItemStorage;
+//                break;
+//            }
+//        }
+        foodItem = itemStorage.getFoodItemByCode(foodItemCode);
+        if (foodItem == null) {
+            throw new cucumber.api.PendingException(foodItemCode + " has not been added to the database");
+        } else {
+            String moneyNZD = "NZD ";
+            foodItemCost = BigMoney.parse(moneyNZD.concat(Integer.toString(count * foodItem.getCost().getAmountMajorInt())));
         }
+//        if (!valid) {
+//            throw new cucumber.api.PendingException(valid + " has not been added to the database");
+//        } else {
+//            expectedCost.multipliedBy(count);
+//        }
     }
 
-    @Then("the system adds the new data to the current data")
-    public void the_system_adds_the_new_data_to_the_current_data() {
-        // Write code here that turns the phrase above into concrete actions
-        List<FoodItem> items = new ArrayList<FoodItem>(itemStorage.getAllFoodItems());
-        items.sort((item1, item2) -> item1.getCode().compareTo(item2.getCode()));
-
-        switch (testName) {
-            case "TESTXML1":
-                FoodItem beefburg = new FoodItem("BEEFBURG", "Hamburger", UnitType.GRAM);
-                beefburg.setCost(BigMoney.parse("NZD 21.00"));
-                beefburg.setCaloriesPerUnit(295);
-                FoodItem cheeseburg = new FoodItem("CHEESEBURG", "Cheeseburger", UnitType.GRAM);
-                cheeseburg.setCost(BigMoney.parse("NZD 22.00"));
-                cheeseburg.setCaloriesPerUnit(303);
-                FoodItem tofuburg1 = new FoodItem("TOFUBURG", "Tofu Burger", UnitType.GRAM);
-                tofuburg1.setCost(BigMoney.parse("NZD 50.00"));
-                tofuburg1.setCaloriesPerUnit(168.4);
-                tofuburg1.setIsVegetarian(true);
-                FoodItem applesoda = new FoodItem("APPLESODA", "Sparkling Apple", UnitType.ML);
-                applesoda.setCost(BigMoney.parse("NZD 96.00"));
-                applesoda.setCaloriesPerUnit(87);
-                applesoda.setIsVegan(true);
-                applesoda.setIsGlutenFree(true);
-                expectedItems = Arrays.asList(
-                        beefburg, cheeseburg, tofuburg1, applesoda
-                );
-                expectedItems.sort((item1, item2) -> item1.getCode().compareTo(item2.getCode()));
-                break;
-            case "TESTXML2":
-                FoodItem banana = new FoodItem("BANANA", "Banana", UnitType.COUNT);
-                banana.setCost(BigMoney.parse("NZD 1.00"));
-                banana.setCaloriesPerUnit(100);
-                FoodItem applepie = new FoodItem("APPLEPIE", "Apple Pie", UnitType.COUNT);
-                applepie.setCost(BigMoney.parse("NZD 21.00"));
-                applepie.setCaloriesPerUnit(200);
-                FoodItem tofuburg2 = new FoodItem("TOFUBURG", "Tofu Burger", UnitType.GRAM);
-                tofuburg2.setCost(BigMoney.parse("NZD 50.00"));
-                tofuburg2.setCaloriesPerUnit(268.4);
-                tofuburg2.setIsVegetarian(true);
-                FoodItem water = new FoodItem("WATER", "Glass of water", UnitType.ML);
-                water.setCost(BigMoney.parse("NZD 200.00"));
-                water.setCaloriesPerUnit(0);
-                water.setIsVegan(true);
-                water.setIsGlutenFree(true);
-                expectedItems = Arrays.asList(
-                        banana, applepie, tofuburg2, water
-                );
-                expectedItems.sort((item1, item2) -> item1.getCode().compareTo(item2.getCode()));
-                break;
-            default:
-                throw new cucumber.api.PendingException(testName + " is an invalid file");
-        }
-
-        Assertions.assertEquals(items.size(), expectedItems.size());
-        Assertions.assertEquals(items, expectedItems);
-
+    @Then("the total cost is ${int}")
+    public void the_total_cost_is_$(Integer intCost) {
+        String expectedMoneyNZD = "NZD ";
+        BigMoney expectedCostNZD = BigMoney.parse(expectedMoneyNZD.concat(Integer.toString(intCost)));
+        assertEquals(foodItemCost, expectedCostNZD);
     }
+
+
+//    String source;
+//    String testName;
+//    List<FoodItem> expectedItems;
+//    FoodItemDAO itemStorage;
+//
+//    @Given("the user has a data from directory {string} to upload")
+//    public void the_user_has_a_data_from_directory_to_upload(String source) {
+//        this.source = source;
+//        //throw new cucumber.api.PendingException();
+//    }
+//
+//    @And("the file is the right type and correctly formatted")
+//    public void the_file_is_the_right_type_and_correctly_formatted() {
+//        if (source.contains("TESTXML1.xml")) {
+//            testName = "TESTXML1";
+//        } else if (source.contains("TESTXML2.xml")) {
+//            testName = "TESTXML2";
+//        } else {
+//            throw new cucumber.api.PendingException(source + " not a valid directory");
+//        }
+//    }
+//
+//    @When("the user uploads the file")
+//    public void the_user_uploads_the_file() {
+//        itemStorage = DAOFactory.getFoodItemDAO();
+//        DAOFactory.resetInstances();
+//        try {
+//            UploadHandler.uploadFoodItems(source);
+//        } catch (IOException | SAXException e) {
+//            e.printStackTrace();
+//            throw new cucumber.api.PendingException("Failed uploading " + testName);
+//        }
+//    }
+//
+//    @Then("the system adds the new data to the current data")
+//    public void the_system_adds_the_new_data_to_the_current_data() {
+//        // Write code here that turns the phrase above into concrete actions
+//        List<FoodItem> items = new ArrayList<FoodItem>(itemStorage.getAllFoodItems());
+//        items.sort((item1, item2) -> item1.getCode().compareTo(item2.getCode()));
+//
+//        switch (testName) {
+//            case "TESTXML1":
+//                FoodItem beefburg = new FoodItem("BEEFBURG", "Hamburger", UnitType.GRAM);
+//                beefburg.setCost(BigMoney.parse("NZD 21.00"));
+//                beefburg.setCaloriesPerUnit(295);
+//                FoodItem cheeseburg = new FoodItem("CHEESEBURG", "Cheeseburger", UnitType.GRAM);
+//                cheeseburg.setCost(BigMoney.parse("NZD 22.00"));
+//                cheeseburg.setCaloriesPerUnit(303);
+//                FoodItem tofuburg1 = new FoodItem("TOFUBURG", "Tofu Burger", UnitType.GRAM);
+//                tofuburg1.setCost(BigMoney.parse("NZD 50.00"));
+//                tofuburg1.setCaloriesPerUnit(168.4);
+//                tofuburg1.setIsVegetarian(true);
+//                FoodItem applesoda = new FoodItem("APPLESODA", "Sparkling Apple", UnitType.ML);
+//                applesoda.setCost(BigMoney.parse("NZD 96.00"));
+//                applesoda.setCaloriesPerUnit(87);
+//                applesoda.setIsVegan(true);
+//                applesoda.setIsGlutenFree(true);
+//                expectedItems = Arrays.asList(
+//                        beefburg, cheeseburg, tofuburg1, applesoda
+//                );
+//                expectedItems.sort((item1, item2) -> item1.getCode().compareTo(item2.getCode()));
+//                break;
+//            case "TESTXML2":
+//                FoodItem banana = new FoodItem("BANANA", "Banana", UnitType.COUNT);
+//                banana.setCost(BigMoney.parse("NZD 1.00"));
+//                banana.setCaloriesPerUnit(100);
+//                FoodItem applepie = new FoodItem("APPLEPIE", "Apple Pie", UnitType.COUNT);
+//                applepie.setCost(BigMoney.parse("NZD 21.00"));
+//                applepie.setCaloriesPerUnit(200);
+//                FoodItem tofuburg2 = new FoodItem("TOFUBURG", "Tofu Burger", UnitType.GRAM);
+//                tofuburg2.setCost(BigMoney.parse("NZD 50.00"));
+//                tofuburg2.setCaloriesPerUnit(268.4);
+//                tofuburg2.setIsVegetarian(true);
+//                FoodItem water = new FoodItem("WATER", "Glass of water", UnitType.ML);
+//                water.setCost(BigMoney.parse("NZD 200.00"));
+//                water.setCaloriesPerUnit(0);
+//                water.setIsVegan(true);
+//                water.setIsGlutenFree(true);
+//                expectedItems = Arrays.asList(
+//                        banana, applepie, tofuburg2, water
+//                );
+//                expectedItems.sort((item1, item2) -> item1.getCode().compareTo(item2.getCode()));
+//                break;
+//            default:
+//                throw new cucumber.api.PendingException(testName + " is an invalid file");
+//        }
+//
+//        Assertions.assertEquals(items.size(), expectedItems.size());
+//        Assertions.assertEquals(items, expectedItems);
+//
+//    }
 
 
     @Ignore
--- team1-app/src/test/resources/seng202/team1/features/datahandling.feature
+++ team1-app/src/test/resources/seng202/team1/features/datahandling.feature
@@ -1,80 +1,89 @@
 Feature: User handles data using the app
   Data handling
 
+  Scenario Outline: Cost of tofu burgers
+    Given XML file from "src/test/resources/xml/TESTXML1.xml" is uploaded
+    When user wants to know the total cost of <count> "TOFUBURG"
+    Then the total cost is $<total>
+    Examples:
+      | count | total |
+      | 1     |  50   |
+      | 2     |  100  |
 
-  Scenario: User uploads data from file
-    Given the user has a data from directory "src/test/resources/xml/TESTXML1.xml" to upload
-    And the file is the right type and correctly formatted
-    When the user uploads the file
-    Then the system adds the new data to the current data
-    And prompts the user to decide what to keep for each duplicate entry
 
-  Scenario: User uploads data from file
-    Given the user has a data from directory "src/test/resources/xml/TESTXML2.xml" to upload
-    And the file is the right type and correctly formatted
-    When the user uploads the file
-    Then the system adds the new data to the current data
-    And prompts the user to decide what to keep for each duplicate entry
+#  Scenario: User uploads data from file
+#    Given the user has a data from directory "src/test/resources/xml/TESTXML1.xml" to upload
+#    And the file is the right type and correctly formatted
+#    When the user uploads the file
+#    Then the system adds the new data to the current data
+#    And prompts the user to decide what to keep for each duplicate entry
+#
+#  Scenario: User uploads data from file
+#    Given the user has a data from directory "src/test/resources/xml/TESTXML2.xml" to upload
+#    And the file is the right type and correctly formatted
+#    When the user uploads the file
+#    Then the system adds the new data to the current data
+#    And prompts the user to decide what to keep for each duplicate entry
+#
+#  Scenario: User manually enters a new ingredient/food item/supplier data
+#    Given the system is in data entry mode
+#    When the user provides new or updated data in the correct field(s)
+#    Then the system updates the data entry
+#
+#  Scenario: User adds a new food item
+#    Given the system is in the food item entry state
+#    When user adds a new food item
+#    Then system adds new food item to database
+#    And system displays the new food item
+#
+#  Scenario: User deletes existing recipe
+#    Given menu is not empty
+#    When user deletes recipe
+#    Then system removes food item from database
+#
+#  Scenario: User modifies existing recipe
+#    Given menu is not empty2
+#    When user modifies recipe
+#    Then system updates recipe in database
+#
+#  Scenario: User edits an ingredient’s data
+#    Given an ingredient is selected
+#    And the system is showing all available information about the ingredient
+#    When the user changes a specific field
+#    Then the system updates the value to match the user input
+#
+#  Scenario: Compute number of servings that current stocks will allow to be made
+#    Given the current stock is recorded into the database
+#    When user requests remaining serves
+#    Then the application should compute how many servings can be made with the given amount of stock
+#
+#  Scenario: User updates the quantities of ingredients used in a recipe
+#    Given a food item is in the database and is currently selected
+#    When the user changes the quantity value
+#    Then the recipe is updated to display the new quantity
 
-  Scenario: User manually enters a new ingredient/food item/supplier data
-    Given the system is in data entry mode
-    When the user provides new or updated data in the correct field(s)
-    Then the system updates the data entry
+#  Scenario: User adds an existing food item to the current menu
+#    Given the selected food item(s) are not already in the menu
+#    When the user adds a food item to the menu
+#    Then the food item and its details should be displayed on the menu
 
-  Scenario: User adds a new food item
-    Given the system is in the food item entry state
-    When user adds a new food item
-    Then system adds new food item to database
-    And system displays the new food item
-
-  Scenario: User deletes existing recipe
-    Given menu is not empty
-    When user deletes recipe
-    Then system removes food item from database
-
-  Scenario: User modifies existing recipe
-    Given menu is not empty2
-    When user modifies recipe
-    Then system updates recipe in database
-
-  Scenario: User edits an ingredient’s data
-    Given an ingredient is selected
-    And the system is showing all available information about the ingredient
-    When the user changes a specific field
-    Then the system updates the value to match the user input
-
-  Scenario: Compute number of servings that current stocks will allow to be made
-    Given the current stock is recorded into the database
-    When user requests remaining serves
-    Then the application should compute how many servings can be made with the given amount of stock
-
-  Scenario: User updates the quantities of ingredients used in a recipe
-    Given a food item is in the database and is currently selected
-    When the user changes the quantity value
-    Then the recipe is updated to display the new quantity
-
-  Scenario: User adds an existing food item to the current menu
-    Given the selected food item(s) are not already in the menu
-    When the user adds a food item to the menu
-    Then the food item and its details should be displayed on the menu
-
-  Scenario: user needs the lists of data sorted based on their properties
-    Given the items exist in the database
-    And the item has appropriate search/ordering values
-    When the user specifies the attribute to sort by
-    Then the database returns a sorted list of the relevant items
-
-  Scenario: user needs to know when they are running low on ingredients
-    Given the ingredients are registered in the database
-    When the ingredients reach a certain low level
-    Then the user is informed that the ingredient is running low
-
-  Scenario: user wants to see how sales perform under certain conditions
-    Given the tags and time frame are specified
-    When sales are made during the time frame
-    Then the sales data is saved with the property specified
-
-  Scenario: The owner wants to see the sales data in an organised way
-    Given the sales data is recorded
-    When the application is asked to generate a sales report
-    Then the sales data is displayed with visualised sales figures
+#  Scenario: user needs the lists of data sorted based on their properties
+#    Given the items exist in the database
+#    And the item has appropriate search/ordering values
+#    When the user specifies the attribute to sort by
+#    Then the database returns a sorted list of the relevant items
+#
+#  Scenario: user needs to know when they are running low on ingredients
+#    Given the ingredients are registered in the database
+#    When the ingredients reach a certain low level
+#    Then the user is informed that the ingredient is running low
+#
+#  Scenario: user wants to see how sales perform under certain conditions
+#    Given the tags and time frame are specified
+#    When sales are made during the time frame
+#    Then the sales data is saved with the property specified
+#
+#  Scenario: The owner wants to see the sales data in an organised way
+#    Given the sales data is recorded
+#    When the application is asked to generate a sales report
+#    Then the sales data is displayed with visualised sales figures
