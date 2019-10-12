4ff5a1754f0739ec2c6be343522906a8b5636b58
Euan Widjaja <ewi45@uclive.ac.nz>
2019-10-13 11:27:00 +1300

    Added Javadoc for ImportChangesController and removed getOverwrite() and variable overwrite

--- team1-app/src/main/java/seng202/team1/gui/ImportChangesController.java
+++ team1-app/src/main/java/seng202/team1/gui/ImportChangesController.java
@@ -17,18 +17,36 @@ public class ImportChangesController {
     @FXML
     private Button ignoreButton;
 
-    private boolean overwrite;
+//    private boolean overwrite;
+
+    /**
+     * A FoodItemStorageController.
+     * Note that despite being a FoodItemStorageController object,
+     * variable is called importController as this particular functionality
+     * imports data into the storage database, and therefore it is
+     * intuitively named an importController.
+     */
     private FoodItemStorageController importController;
 
     private static final String DESCRIPTION =
             "Uploaded file contains food item that is " +
             "already present in database with different values";
 
+    /**
+     * Called when the accept button is clicked.
+     * Sets the overwrite variable in the FoodItemStorageController variable
+     * importController to true, and then closes the pop up window.
+     */
     public void accept() {
         importController.setOverwrite(true);
         closeWindow(acceptButton);
     }
 
+    /**
+     * Called when the ignore button is clicked.
+     * Sets the overwrite variable in the FoodItemStorageController variable
+     * importController to false, and then closes the pop up window.
+     */
     public void ignore() {
         importController.setOverwrite(false);
         closeWindow(ignoreButton);
@@ -36,23 +54,37 @@ public class ImportChangesController {
 
 
     /**
-     * runs automatically when the window is created
+     * Runs automatically when the window is created
+     */
+
+    /**
+     * Runs when the pop upwindow is created. Takes a FoodItemStorageController
+     * for usage in setting the variable 'overwrite' in the aforementioned class.
+     * @param importController a FoodItemStorageController
      */
     public void initialize(FoodItemStorageController importController) {
         this.importController = importController;
         descriptionLabel.setText(DESCRIPTION);
-
     }
 
+    /**
+     * Closes the pop up window.
+     * @param node a node that represents the window
+     */
     private void closeWindow(Node node) {
         Stage stage  = (Stage) node.getScene().getWindow();
         stage.close();
     }
 
-    public boolean getOverwrite() {
-        return overwrite;
-    }
+//    public boolean getOverwrite() {
+//        return overwrite;
+//    }
 
+    /**
+     * Called from FoodItemStorageController to set the variable importController
+     * to the FoodItemStorageController itself.
+     * @param importController a FoodItemStorageController that handles import of data
+     */
     public void setImportController(FoodItemStorageController importController) {
         this.importController = importController;
     }
